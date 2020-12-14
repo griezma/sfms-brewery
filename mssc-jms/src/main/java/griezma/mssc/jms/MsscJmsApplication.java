29c183bff56cfcd4540af2f27042bf12224428b6
griezma <griezma@outlook.com>
2020-12-14 10:19:39 +0100

    initial

---
+++ mssc-jms/src/main/java/griezma/mssc/jms/MsscJmsApplication.java
@@ -0,0 +1,13 @@
+package griezma.mssc.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscJmsApplication.class, args);
+    }
+
+}
---
+++ mssc-jms/src/test/java/griezma/mssc/jms/MsscJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package griezma.mssc.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
